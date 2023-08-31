5c733073083aa3025984a951ce38cd314e8c8cfb
DevExpressExampleBot <fakeEmail>
2023-08-31 17:21:45 +0400

    Source auto update [skip ci]

--- VB/DetailsUnderEachOther/Form1.Designer.vb
+++ VB/DetailsUnderEachOther/Form1.Designer.vb
@@ -1,5 +1,7 @@
-﻿Namespace DetailsUnderEachOther
-    Partial Public Class Form1
+Namespace DetailsUnderEachOther
+
+    Partial Class Form1
+
         ''' <summary>
         ''' Required designer variable.
         ''' </summary>
@@ -10,28 +12,28 @@
         ''' </summary>
         ''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         Protected Overrides Sub Dispose(ByVal disposing As Boolean)
-            If disposing AndAlso (components IsNot Nothing) Then
-                components.Dispose()
+            If disposing AndAlso (Me.components IsNot Nothing) Then
+                Me.components.Dispose()
             End If
+
             MyBase.Dispose(disposing)
         End Sub
 
-        #Region "Windows Form Designer generated code"
-
+#Region "Windows Form Designer generated code"
         ''' <summary>
         ''' Required method for Designer support - do not modify
         ''' the contents of this method with the code editor.
         ''' </summary>
         Private Sub InitializeComponent()
             Me.components = New System.ComponentModel.Container()
-            Dim gridLevelNode1 As New DevExpress.XtraGrid.GridLevelNode()
-            Dim gridLevelNode2 As New DevExpress.XtraGrid.GridLevelNode()
-            Dim gridLevelNode3 As New DevExpress.XtraGrid.GridLevelNode()
+            Dim gridLevelNode1 As DevExpress.XtraGrid.GridLevelNode = New DevExpress.XtraGrid.GridLevelNode()
+            Dim gridLevelNode2 As DevExpress.XtraGrid.GridLevelNode = New DevExpress.XtraGrid.GridLevelNode()
+            Dim gridLevelNode3 As DevExpress.XtraGrid.GridLevelNode = New DevExpress.XtraGrid.GridLevelNode()
             Me.gvRelations = New DevExpress.XtraGrid.Views.Grid.GridView()
             Me.gridColumn1 = New DevExpress.XtraGrid.Columns.GridColumn()
             Me.gridControl1 = New DevExpress.XtraGrid.GridControl()
             Me.dataSet1BindingSource = New System.Windows.Forms.BindingSource(Me.components)
-            Me.dataSet1_Renamed = New DetailsUnderEachOther.DataSet1()
+            Me.dataSet1 = New DetailsUnderEachOther.DataSet1()
             Me.gvMaster = New DevExpress.XtraGrid.Views.Grid.GridView()
             Me.colID = New DevExpress.XtraGrid.Columns.GridColumn()
             Me.colItemName = New DevExpress.XtraGrid.Columns.GridColumn()
@@ -44,24 +46,28 @@
             Me.colID_A = New DevExpress.XtraGrid.Columns.GridColumn()
             Me.colMasterID_A = New DevExpress.XtraGrid.Columns.GridColumn()
             Me.colColA = New DevExpress.XtraGrid.Columns.GridColumn()
-            DirectCast(Me.gvRelations, System.ComponentModel.ISupportInitialize).BeginInit()
-            DirectCast(Me.gridControl1, System.ComponentModel.ISupportInitialize).BeginInit()
-            DirectCast(Me.dataSet1BindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
-            DirectCast(Me.dataSet1_Renamed, System.ComponentModel.ISupportInitialize).BeginInit()
-            DirectCast(Me.gvMaster, System.ComponentModel.ISupportInitialize).BeginInit()
-            DirectCast(Me.gridView3, System.ComponentModel.ISupportInitialize).BeginInit()
-            DirectCast(Me.gridView2, System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((Me.gvRelations), System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((Me.gridControl1), System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((Me.dataSet1BindingSource), System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((Me.dataSet1), System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((Me.gvMaster), System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((Me.gridView3), System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((Me.gridView2), System.ComponentModel.ISupportInitialize).BeginInit()
             Me.SuspendLayout()
             ' 
             ' gvRelations
             ' 
-            Me.gvRelations.Columns.AddRange(New DevExpress.XtraGrid.Columns.GridColumn() { Me.gridColumn1})
+            Me.gvRelations.Columns.AddRange(New DevExpress.XtraGrid.Columns.GridColumn() {Me.gridColumn1})
             Me.gvRelations.GridControl = Me.gridControl1
             Me.gvRelations.Name = "gvRelations"
             Me.gvRelations.OptionsDetail.ShowDetailTabs = False
             Me.gvRelations.OptionsView.ShowColumnHeaders = False
             Me.gvRelations.OptionsView.ShowGroupPanel = False
             Me.gvRelations.OptionsView.ShowIndicator = False
+            AddHandler Me.gvRelations.MasterRowGetRelationName, New DevExpress.XtraGrid.Views.Grid.MasterRowGetRelationNameEventHandler(AddressOf Me.gvRelations_MasterRowGetRelationName)
+            AddHandler Me.gvRelations.MasterRowEmpty, New DevExpress.XtraGrid.Views.Grid.MasterRowEmptyEventHandler(AddressOf Me.gvRelations_MasterRowEmpty)
+            AddHandler Me.gvRelations.MasterRowGetChildList, New DevExpress.XtraGrid.Views.Grid.MasterRowGetChildListEventHandler(AddressOf Me.gvRelations_MasterRowGetChildList)
+            AddHandler Me.gvRelations.MasterRowGetRelationCount, New DevExpress.XtraGrid.Views.Grid.MasterRowGetRelationCountEventHandler(AddressOf Me.gvRelations_MasterRowGetRelationCount)
             ' 
             ' gridColumn1
             ' 
@@ -81,33 +87,37 @@
             gridLevelNode2.RelationName = "MasterTable_DetailA"
             gridLevelNode3.LevelTemplate = Me.gridView3
             gridLevelNode3.RelationName = "MasterTable_DetailB"
-            gridLevelNode1.Nodes.AddRange(New DevExpress.XtraGrid.GridLevelNode() { gridLevelNode2, gridLevelNode3})
+            gridLevelNode1.Nodes.AddRange(New DevExpress.XtraGrid.GridLevelNode() {gridLevelNode2, gridLevelNode3})
             gridLevelNode1.RelationName = "Relations"
-            Me.gridControl1.LevelTree.Nodes.AddRange(New DevExpress.XtraGrid.GridLevelNode() { gridLevelNode1})
+            Me.gridControl1.LevelTree.Nodes.AddRange(New DevExpress.XtraGrid.GridLevelNode() {gridLevelNode1})
             Me.gridControl1.Location = New System.Drawing.Point(0, 0)
             Me.gridControl1.MainView = Me.gvMaster
             Me.gridControl1.Name = "gridControl1"
             Me.gridControl1.Size = New System.Drawing.Size(599, 416)
             Me.gridControl1.TabIndex = 0
-            Me.gridControl1.ViewCollection.AddRange(New DevExpress.XtraGrid.Views.Base.BaseView() { Me.gvMaster, Me.gridView3, Me.gridView2, Me.gvRelations})
+            Me.gridControl1.ViewCollection.AddRange(New DevExpress.XtraGrid.Views.Base.BaseView() {Me.gvMaster, Me.gridView3, Me.gridView2, Me.gvRelations})
             ' 
             ' dataSet1BindingSource
             ' 
             Me.dataSet1BindingSource.DataMember = "MasterTable"
-            Me.dataSet1BindingSource.DataSource = Me.dataSet1_Renamed
+            Me.dataSet1BindingSource.DataSource = Me.dataSet1
             ' 
             ' dataSet1
             ' 
-            Me.dataSet1_Renamed.DataSetName = "DataSet1"
-            Me.dataSet1_Renamed.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
+            Me.dataSet1.DataSetName = "DataSet1"
+            Me.dataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
             ' 
             ' gvMaster
             ' 
-            Me.gvMaster.Columns.AddRange(New DevExpress.XtraGrid.Columns.GridColumn() { Me.colID, Me.colItemName})
+            Me.gvMaster.Columns.AddRange(New DevExpress.XtraGrid.Columns.GridColumn() {Me.colID, Me.colItemName})
             Me.gvMaster.GridControl = Me.gridControl1
             Me.gvMaster.Name = "gvMaster"
             Me.gvMaster.OptionsDetail.ShowDetailTabs = False
             Me.gvMaster.OptionsView.ShowGroupPanel = False
+            AddHandler Me.gvMaster.MasterRowGetRelationName, New DevExpress.XtraGrid.Views.Grid.MasterRowGetRelationNameEventHandler(AddressOf Me.gvMaster_MasterRowGetRelationName)
+            AddHandler Me.gvMaster.MasterRowEmpty, New DevExpress.XtraGrid.Views.Grid.MasterRowEmptyEventHandler(AddressOf Me.gvMaster_MasterRowEmpty)
+            AddHandler Me.gvMaster.MasterRowGetChildList, New DevExpress.XtraGrid.Views.Grid.MasterRowGetChildListEventHandler(AddressOf Me.gvMaster_MasterRowGetChildList)
+            AddHandler Me.gvMaster.MasterRowGetRelationCount, New DevExpress.XtraGrid.Views.Grid.MasterRowGetRelationCountEventHandler(AddressOf Me.gvMaster_MasterRowGetRelationCount)
             ' 
             ' colID
             ' 
@@ -127,7 +137,7 @@
             ' 
             ' gridView3
             ' 
-            Me.gridView3.Columns.AddRange(New DevExpress.XtraGrid.Columns.GridColumn() { Me.colID_B, Me.colMasterID_B, Me.colColB1, Me.colColB2})
+            Me.gridView3.Columns.AddRange(New DevExpress.XtraGrid.Columns.GridColumn() {Me.colID_B, Me.colMasterID_B, Me.colColB1, Me.colColB2})
             Me.gridView3.GridControl = Me.gridControl1
             Me.gridView3.Name = "gridView3"
             Me.gridView3.OptionsView.ShowGroupPanel = False
@@ -166,7 +176,7 @@
             ' 
             ' gridView2
             ' 
-            Me.gridView2.Columns.AddRange(New DevExpress.XtraGrid.Columns.GridColumn() { Me.colID_A, Me.colMasterID_A, Me.colColA})
+            Me.gridView2.Columns.AddRange(New DevExpress.XtraGrid.Columns.GridColumn() {Me.colID_A, Me.colMasterID_A, Me.colColA})
             Me.gridView2.GridControl = Me.gridControl1
             Me.gridView2.Name = "gridView2"
             Me.gridView2.OptionsView.ShowGroupPanel = False
@@ -203,37 +213,50 @@
             Me.Controls.Add(Me.gridControl1)
             Me.Name = "Form1"
             Me.Text = "Form1"
-            DirectCast(Me.gvRelations, System.ComponentModel.ISupportInitialize).EndInit()
-            DirectCast(Me.gridControl1, System.ComponentModel.ISupportInitialize).EndInit()
-            DirectCast(Me.dataSet1BindingSource, System.ComponentModel.ISupportInitialize).EndInit()
-            DirectCast(Me.dataSet1_Renamed, System.ComponentModel.ISupportInitialize).EndInit()
-            DirectCast(Me.gvMaster, System.ComponentModel.ISupportInitialize).EndInit()
-            DirectCast(Me.gridView3, System.ComponentModel.ISupportInitialize).EndInit()
-            DirectCast(Me.gridView2, System.ComponentModel.ISupportInitialize).EndInit()
+            AddHandler Me.Load, New System.EventHandler(AddressOf Me.Form1_Load)
+            CType((Me.gvRelations), System.ComponentModel.ISupportInitialize).EndInit()
+            CType((Me.gridControl1), System.ComponentModel.ISupportInitialize).EndInit()
+            CType((Me.dataSet1BindingSource), System.ComponentModel.ISupportInitialize).EndInit()
+            CType((Me.dataSet1), System.ComponentModel.ISupportInitialize).EndInit()
+            CType((Me.gvMaster), System.ComponentModel.ISupportInitialize).EndInit()
+            CType((Me.gridView3), System.ComponentModel.ISupportInitialize).EndInit()
+            CType((Me.gridView2), System.ComponentModel.ISupportInitialize).EndInit()
             Me.ResumeLayout(False)
-
         End Sub
 
-        #End Region
-
+#End Region
         Friend gridControl1 As DevExpress.XtraGrid.GridControl
-        Private WithEvents gvMaster As DevExpress.XtraGrid.Views.Grid.GridView
+
+        Private gvMaster As DevExpress.XtraGrid.Views.Grid.GridView
+
         Private dataSet1BindingSource As System.Windows.Forms.BindingSource
 
-        Private dataSet1_Renamed As DataSet1
+        Private dataSet1 As DetailsUnderEachOther.DataSet1
+
         Private gridView2 As DevExpress.XtraGrid.Views.Grid.GridView
+
         Private colID_A As DevExpress.XtraGrid.Columns.GridColumn
+
         Private colMasterID_A As DevExpress.XtraGrid.Columns.GridColumn
+
         Private colColA As DevExpress.XtraGrid.Columns.GridColumn
+
         Private gridView3 As DevExpress.XtraGrid.Views.Grid.GridView
+
         Private colID_B As DevExpress.XtraGrid.Columns.GridColumn
+
         Private colMasterID_B As DevExpress.XtraGrid.Columns.GridColumn
+
         Private colColB1 As DevExpress.XtraGrid.Columns.GridColumn
+
         Private colColB2 As DevExpress.XtraGrid.Columns.GridColumn
-        Private WithEvents gvRelations As DevExpress.XtraGrid.Views.Grid.GridView
+
+        Private gvRelations As DevExpress.XtraGrid.Views.Grid.GridView
+
         Private gridColumn1 As DevExpress.XtraGrid.Columns.GridColumn
+
         Private colID As DevExpress.XtraGrid.Columns.GridColumn
+
         Private colItemName As DevExpress.XtraGrid.Columns.GridColumn
     End Class
 End Namespace
-
